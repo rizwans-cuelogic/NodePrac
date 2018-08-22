fda8fd39f9cb91deae15d1b387d0e39db3a0a51f
rizwan <rizwan.shaikh@cuelogic.com>
2018-08-22 23:02:51 +0530

    added https server

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req,res)=>{
+// const server = http.createServer((req,res)=>{
 
-    if (req.url == '/'){
-        res.writeHead(200,{'content-type':'text/plain'});
-        res.end("Hello this is root");
+//     if (req.url == '/'){
+//         res.writeHead(200,{'content-type':'text/plain'});
+//         res.end("Hello this is root");
+//     }
+//     else if (req.url =='/rizwan'){
+//         res.writeHead(200,{'content-type':'application/json'});
+//         res.end(JSON.stringify({"name":"rizwan","age":"24","sex":"male"}));
+//     }
+
+// })
+
+// server.listen(9000);
+
+// console.log("Server listening on 9000");
+
+const https = require('https');
+const pem = require('pem');
+
+pem.createCertificate({days:1,selfSigned:true},(err,keys)=>{
+    if (err) {
+        throw err
     }
-    else if (req.url =='/rizwan'){
-        res.writeHead(200,{'content-type':'application/json'});
-        res.end(JSON.stringify({"name":"rizwan","age":"24","sex":"male"}));
-    }
+    https.createServer({key:keys.serviceKey,
+                        cert:keys.certificate},
+                        (req,res)=>{
+                            if(req.url=='/'){
+                                res.writeHead(200,{'content-type':'text/plain'})
+                                res.end(JSON.stringify("hello this is me"));
+                            }
+                        }).listen(5000);
 
-})
-
-server.listen(9000);
-
-console.log("Server listening on 9000");
+});
